06d3bc1a53640201c9581906fcd583538d6c977d
Jin Bok de Oliveira <jinoliveira74@gmail.com>
2021-09-19 14:46:06 -0300

    chore: Autenticação com firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+import { AuthProvider } from './contexts/AuthContexts';
+
+import './styles/global.scss'
 
 function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+   return (
+      <BrowserRouter>
+         <AuthProvider>
+            <Route path="/" exact component={Home}/>
+            <Route path="/rooms/new" component={NewRoom}/>
+         </AuthProvider>
+      </BrowserRouter>
+   );
 }
 
 export default App;
